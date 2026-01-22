682abaaf04cc58ca3648b1d920ed1112de1e8bcc
Tasnim Chaoui <tasnimchaoui9@gmail.com>
2026-01-22 15:32:21 +0100

    Swagger config & controller annotation

--- src/main/java/com/tarmiz/SIRH_backend/controller/EmployeeController.java
+++ src/main/java/com/tarmiz/SIRH_backend/controller/EmployeeController.java
@@ -5,6 +5,10 @@ import com.tarmiz.SIRH_backend.model.DTO.EmployeeDetailsDTO;
 import com.tarmiz.SIRH_backend.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,26 +16,53 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+
+@Tag(
+        name = "Employees",
+        description = "Employee management operations"
+)
 @RestController
 @RequestMapping("/hrEmployees")
 @RequiredArgsConstructor
-@Tag(name = "Employee", description = "APIs for managing employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
     @GetMapping("/{id}")
     @Operation(
-            summary = "Get employee details by ID",
-            description = "Retrieve detailed information of an employee using their unique ID"
+            summary = "Get employee's details by ID",
+            description = "Retrieve detailed information of an employee using their unique ID without contract information "
     )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Employee found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDetailsDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Employee not found"
+            )
+    })
     public ResponseEntity<EmployeeDetailsDTO> getEmployeeDetails(
-            @Parameter(description = "ID of the employee to retrieve", required = true)
+            @Parameter(description = "ID of the employee to retrieve", example = "1",required = true)
             @PathVariable Long id) {
         EmployeeDetailsDTO dto = employeeService.getEmployeeDetails(id);
         return ResponseEntity.ok(dto);
     }
 
+
+    @Operation(
+            summary = "Get paginated employees list",
+            description = "Returns a paginated list of employees with optional sorting"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Employees list retrieved successfully"
+    )
     @GetMapping
     public ResponseEntity<Map<String, Object>> getEmployeesList(
             @RequestParam(defaultValue = "0") int start,
@@ -43,6 +74,15 @@ public class EmployeeController {
         return ResponseEntity.ok(response);
     }
 
+
+    @Operation(
+            summary = "Create a new employee",
+            description = "Creates a new employee with basic information only (no validation or exception handling)"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Employee created successfully"
+    )
     @PostMapping
     public ResponseEntity<?> createEmployee(@RequestBody EmployeeCreateDTO dto) {
         Map<String, Object> response = employeeService.createEmployee(dto);
